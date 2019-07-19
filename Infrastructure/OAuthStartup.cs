34736da842cef1ef4b7e68b18408b87d92c4c571
mynkow <mynkow@gmail.com>
2019-07-19 23:52:26 +0300

    Sets UseForwardedHeaders. It now sends all the requests as HTTPS.

---
+++ Infrastructure/OAuthStartup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nop.Core.Infrastructure;
+
+namespace Nop.Plugin.ExternalAuth.OAuth2.Infrastructure
+{
+    public class OAuthStartup : INopStartup
+    {
+        public int Order => 101112;
+
+        public void Configure(IApplicationBuilder application)
+        {
+            application.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedProto
+            });
+        }
+
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}
